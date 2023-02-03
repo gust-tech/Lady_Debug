d35715ab5fc76fc9375e9674b2244a69387f8c01
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2023-02-03 17:00:33 -0300

    Update Service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://dev-girls-ny8d.onrender.com'
 })
 
     export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
