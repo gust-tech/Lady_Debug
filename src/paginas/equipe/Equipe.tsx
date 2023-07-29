194a7be24903e771ad63e0779c7f83c650f26327
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2023-07-29 15:09:35 -0300

    fix photo/links

--- src/paginas/equipe/Equipe.tsx
+++ src/paginas/equipe/Equipe.tsx
@@ -26,7 +26,7 @@ function Equipe() {
                         data-canonical-src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&amp;logo=github&amp;logoColor=white" alt=''/>
                 </a>
 
-                    <img src="https://media.licdn.com/dms/image/C4D03AQFtenqlYOWEXA/profile-displayphoto-shrink_800_800/0/1663332513743?e=1695254400&v=beta&t=lKsemogDzD3VByVMBwuO97H4RxNjh6mPPdxl2FHiJjs" 
+                    <img src="https://media.licdn.com/dms/image/D4D03AQFMFQJWs37Rug/profile-displayphoto-shrink_800_800/0/1689863726462?e=1695859200&v=beta&t=kuQ-SNGUjeSUrrtpVRuX6rkmn5qn6cAMO4F5RNI8u-Q" 
                     className= 'tamanhos imagemequipe' alt=''
                      />    
                 <li className='fontequipe tipofonteq'>Raquel Soares</li>
@@ -44,7 +44,7 @@ function Equipe() {
                     className= 'tamanhos imagemequipe' alt=''/>       
                 
                 <li className='fontequipe tipofonteq'>Gustavo Rocha</li>
-                <a href="https://www.linkedin.com/in/gustavo-rocha-860706250/" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/gust-tech" target="_blank" rel="noopener noreferrer">
                     <img src="https://camo.githubusercontent.com/c00f87aeebbec37f3ee0857cc4c20b21fefde8a96caf4744383ebfe44a47fe3f/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d4c696e6b6564496e2d2532333030373742353f7374796c653d666f722d7468652d6261646765266c6f676f3d6c696e6b6564696e266c6f676f436f6c6f723d7768697465"
                         data-canonical-src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&amp;logo=linkedin&amp;logoColor=white" alt=''/>
                 </a>
@@ -58,7 +58,7 @@ function Equipe() {
                     className= 'tamanhos imagemequipe' alt=''/>    
                 
                 <li className='fontequipe tipofonteq'>Lucas Campos</li>
-                <a href="https://www.linkedin.com/in/gustavo-rocha-860706250/" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
                     <img src="https://camo.githubusercontent.com/c00f87aeebbec37f3ee0857cc4c20b21fefde8a96caf4744383ebfe44a47fe3f/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d4c696e6b6564496e2d2532333030373742353f7374796c653d666f722d7468652d6261646765266c6f676f3d6c696e6b6564696e266c6f676f436f6c6f723d7768697465"
                         data-canonical-src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&amp;logo=linkedin&amp;logoColor=white" alt=''/>
                 </a>
